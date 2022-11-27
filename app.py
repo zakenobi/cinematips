9ab79644ea226e418cf7aadb1d08439def1f78be
zac <zachary.gagnou@epfedu.fr>
2022-11-27 08:50:12 +0100

    Improvments

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-title = st.text_input('Movie title', 'Life of Brian')
+title = st.text_input('Movie title')
 st.write('The current movie title is', title)
 
 movies = ImdbRequest.get_movies(search=title)
@@ -12,12 +12,9 @@ movies = ImdbRequest.get_movies(search=title)
 movies.sort(key=lambda x: x.ratingRottenTomatoes, reverse=True)
 
 for movie in movies:
-    print(f"{movie.ratingRottenTomatoes} : {movie.title}")
-
-for movie in movies:
-    st.caption(movie.title)
+    st.header(movie.title)
     st.code(movie.ratingRottenTomatoes)
 
     response = requests.get(movie.image)
     img = Image.open(BytesIO(response.content))
-    st.image(img, caption='Sunrise by the mountains')
+    st.image(img)
