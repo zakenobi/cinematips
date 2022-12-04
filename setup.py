ed12b9263e5bd1e612f2dd809ad51873573e32a9
zac <zachary.gagnou@epfedu.fr>
2022-12-04 22:10:37 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 setup(
@@ -6,6 +10,8 @@ setup(
     version='0.1.0',
     author='Zachary Gagnou & Alexandra Mille-Egea',
     url='https://github.com/zakenobi/cinematips',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(include=['cinematips', 'cinematips.*']),
     install_requires=[
         'requests==2.28.1',
