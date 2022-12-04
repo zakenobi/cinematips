f07a10a2a07a9ee24bf8dba738e040a4712d6f26
zac <zachary.gagnou@epfedu.fr>
2022-12-04 21:58:16 +0100

    Tag test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='cinematips',
     version='0.1.0',
-    author='Zachary Gagnou and Alexandra Mille-Egea',
+    author='Zachary Gagnou & Alexandra Mille-Egea',
     url='https://github.com/zakenobi/cinematips',
     packages=find_packages(include=['cinematips', 'cinematips.*']),
     install_requires=[
