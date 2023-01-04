a0570a8ba6c1c6e921c6afc0fd5cf90beecece68
zac <zachary.gagnou@epfedu.fr>
2023-01-04 10:47:38 +0100

    Update req

--- pages/trends.py
+++ pages/trends.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 # make a request to google trends comparing the search terms "Boeing" and "SpaceX" in the US
 pytrends = TrendReq(hl='en-US', tz=360)
-kw_list = ["Boeing", "SpaceX"]
+kw_list = ["Boeing", "SpaceX", "NASA"]
 pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo='US', gprop='')
 df = pytrends.interest_over_time()
 df = df.drop(labels=['isPartial'],axis='columns')
+
+# Add a title to the page
+st.title("Google Trends of Boeing vs SpaceX vs NASA in the US")
 
 # Plot the data in a line chart using streamlit
 st.line_chart(df)
